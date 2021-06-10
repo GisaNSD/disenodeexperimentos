db8c47dbe1aaa239a862c234a17a0f4cdc3bad31
GisaNSD <gisi_tina4@hotmail.com>
2021-06-10 23:27:13 +0200

    First button responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,10 @@
 <x-head />
 <body>
     <div class="flex justify-center flex-col">
-        <h1 class="sm:text-3xl lg:text-6xl text-gray-600 text-center lg:my-32 ">Elija su fase y su grupo.</h1>
+        <h1 class="sm:my-20 text-6xl text-gray-600 text-center lg:my-32 ">Elija su fase y su grupo.</h1>
         <div class="flex mx-auto">
             <div>
-                <button id="fase1" class="flex sm:mx-20 sm:py-1 sm:text-xl lg:mx-32 lg:px-20 lg:py-3 lg:text-3xl text-gray-button transition duration-500 ease-in-out bg-blue-button hover:bg-gray-button hover:text-blue-button transform hover:-translate-y-1 hover:scale-110 ">Fase 1</button>
+                <button id="fase1" class="flex sm:px-10 sm:text-xl mx-32 lg:px-20 py-3 lg:text-3xl text-gray-button transition duration-500 ease-in-out bg-blue-button hover:bg-gray-button hover:text-blue-button transform hover:-translate-y-1 hover:scale-110 ">Fase 1</button>
                 
                 <div id="groups1" class="flex justify-center mx-automy-12 my-5 animate-fade-in-down hidden">
                     <a href="{{route('fase1', 1)}}">
