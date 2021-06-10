d622b57c4068cf5b184474599abd3a7874f045cd
GisaNSD <gisi_tina4@hotmail.com>
2021-06-11 01:29:40 +0200

    Responsive results

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,8 @@
 Swal.fire({
             title: 'Guía de uso',
-            text: 'Por favor, puntúe las imágenes en función de su agrado y siguiendo el orden en el que aparecen.Para puntuar, arrastre el botón de la barra medidora hasta el valor deseado. Al terminar, deberá proporcionarle el valor que le aparezca por pantalla a la persona que le ha mandado esta página. Junto al valor deberá comunicar su fase y número de grupo.',
+            text: 'Por favor, puntúe las imágenes en función de su agrado y siguiendo el orden en el que aparecen.
+            Para puntuar, arrastre el botón de la barra medidora hasta el valor deseado. 
+            Al terminar, deberá proporcionarle el valor que le aparezca por pantalla a la persona que le ha mandado esta página. Junto al valor deberá comunicar su fase y número de grupo.',
             icon: 'question',
             confirmButtonText: '¡Entendido!'
         })
