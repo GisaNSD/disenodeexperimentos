e30ba6874eb89bb75d6ae42e79e6df33fc6f1192
GisaNSD <gisi_tina4@hotmail.com>
2021-06-11 12:22:57 +0200

    Numbers cant be selected

--- resources/views/components/numbers-range.blade.php
+++ resources/views/components/numbers-range.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-around">
+<div class="flex justify-around select-none">
                 <p class="inline-block">0</p>
                 <p class="inline-block">10</p>
 </div>
